388bcb262d64d9ff9301769038cded3dd63d5b1f
Magalame <12373104+Magalame@users.noreply.github.com>
2019-04-17 17:44:53 -0400

    wrong call about 3.7

--- ping_dnscrypt.py
+++ ping_dnscrypt.py
@@ -6,7 +6,7 @@ import struct
 import select
 import socket
 import sys
-import urllib
+import urllib.request
 from threading import Lock,Thread
 import argparse
 from timeit import default_timer as timer
@@ -450,7 +450,7 @@ def pingDnscryptFile(filename):
 def pingDnscrypt():
 
     if not os.path.isfile("dnscrypt-resolvers.csv"):
-        urllib.urlretrieve("https://raw.githubusercontent.com/dyne/dnscrypt-proxy/master/dnscrypt-resolvers.csv","dnscrypt-resolvers.csv")
+        urllib.request.urlretrieve("https://raw.githubusercontent.com/dyne/dnscrypt-proxy/master/dnscrypt-resolvers.csv","dnscrypt-resolvers.csv")
     pingDnscryptFile("dnscrypt-resolvers.csv")
 
 
